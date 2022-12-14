b586ca610aed19b3db3f576de0b9eccb33283125
nourman23 <nourmanzawahreh@gmail.com>
2022-12-14 22:14:30 +0300

    user avatar

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,25 +1,81 @@
 /* eslint-disable */
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useIsAuthenticated, useSignIn } from "react-auth-kit";
 import { useNavigate } from "react-router-dom";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { storage } from "./firebase";
 
 export const Register = (props) => {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
   const [name, setName] = useState("");
+  // const [file, setFile] = useState();
+
+  const [imgUrl, setImgUrl] = useState(null);
+  const [progresspercent, setProgresspercent] = useState(0);
+  const inputRef = useRef(null);
+
   const isAuth = useIsAuthenticated();
   const navigate = useNavigate();
   const signIn = useSignIn();
+  const handleClick = () => {
+    // 👇️ open file input box on click of other element
+    inputRef.current.click();
+  };
+  const handleFileChange = (event) => {
+    const fileObj = event.target.files && event.target.files[0];
+    if (!fileObj) {
+      return;
+    }
+
+    // console.log('fileObj is', fileObj);
+
+    // 👇️ reset file input
+    event.target.value = null;
+
+    // 👇️ is now empty
+    // console.log(event.target.files);
+
+    // 👇️ can still access file object here
+    // console.log(fileObj);
+    // console.log(fileObj.name);
+
+    const storageRef = ref(storage, `files/${fileObj.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, fileObj);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgresspercent(progress);
+        console.log(progress);
+      },
+      (error) => {
+        alert(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImgUrl(downloadURL);
+
+          console.log(downloadURL);
+          //TODO: save url to database
+        });
+      }
+    );
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email);
     const data = new FormData(e.currentTarget);
+
     const registerData = {
       name: data.get("name"),
       email: data.get("email"),
       password: data.get("password"),
+      avatar: imgUrl,
     };
     console.log(registerData);
 
@@ -52,6 +108,18 @@ export const Register = (props) => {
         <h2 className="text-3xl font-semibold text-center uppercase ContactForm mt-6">
           Register
         </h2>
+        <img
+          onClick={handleClick}
+          alt="..."
+          src={imgUrl}
+          className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
+        />
+        <input
+          style={{ display: "none" }}
+          ref={inputRef}
+          type="file"
+          onChange={handleFileChange}
+        />
         <form className="register-form mt-6" onSubmit={handleSubmit}>
           <label htmlFor="name">Full name</label>
           <input
@@ -94,6 +162,29 @@ export const Register = (props) => {
 
         "
           />
+          {/* <label htmlFor="avatar">Your Avatar</label>
+          <input
+            type="file"
+            onChange={(e) => {
+              // console.log(e.target.files[0]);
+              setFile(e.target.files[0]);
+            }}
+            id="avatar"
+            name="avatar"
+            required
+            className="
+          w-full
+          block px-16 py-2 mt-2
+          border-gray-300
+          rounded-md
+          shadow-sm
+          focus:border-indigo-300
+          focus:ring
+          focus:ring-indigo-200
+          focus:ring-opacity-50
+
+        "
+          /> */}
           <label htmlFor="password">password</label>
           <input
             value={pass}
