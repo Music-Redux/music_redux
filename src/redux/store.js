46d157c0a46d9477816ac83922e631fca6e10879
nourman23 <nourmanzawahreh@gmail.com>
2022-12-14 16:50:39 +0300

    more for auth user

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,7 @@ import playerReducer from "./features/playerSlice";
 import reviewReducer from "./fromDB/reviewSlice";
 import userReducer from "./fromDB/userSlice";
 import postReducer from "./fromDB/postSlice";
+import commentReducer from "./fromDB/commentSlice";
 
 export const store = configureStore({
   reducer: {
@@ -13,6 +14,7 @@ export const store = configureStore({
     Reviews: reviewReducer,
     Users: userReducer,
     Posts: postReducer,
+    Comments: commentReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(shazamCoreApi.middleware),
